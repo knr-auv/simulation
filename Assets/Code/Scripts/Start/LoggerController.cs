1cd9b448a603712f0f0ef9473c51e0d78cc88390
Vectro <miki87278@gmail.com>
2020-12-15 16:58:59 +0100

    Added error logging

---
+++ Assets/Code/Scripts/Start/LoggerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LoggerController : MonoBehaviour
+{
+    void Awake()
+    {
+        Application.logMessageReceivedThreaded += HandleLogMessage;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void HandleLogMessage(string logString, string stackTrace, LogType type)
+    {
+        if (type == LogType.Exception || type == LogType.Error)
+            File.AppendAllText("error.log", "[" + DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss") + "] " + logString + "\n" + stackTrace + "\n");
+        if(Settings.config != null && Settings.config.loggingEnabled && type == LogType.Log)
+            File.AppendAllText("debug.log", "[" + DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss") + "] " + logString + "\n" + stackTrace + "\n");
+    }
+}
