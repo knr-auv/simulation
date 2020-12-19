ceae687e74b47db764a8565b12696c8bdc353d88
Vectro <miki87278@gmail.com>
2020-12-19 13:42:25 +0100

    Improved debug rect

--- Assets/Code/Scripts/DatasetGeneration/DetectionRectDraw.cs
+++ Assets/Code/Scripts/DatasetGeneration/DetectionRectDraw.cs
@@ -17,7 +17,7 @@ public class DetectionRectDraw : MonoBehaviour
         detector = new Detector();
         style = new GUIStyle()
         {
-            fontSize = 20,
+            fontSize = 15,
             fontStyle = FontStyle.Bold
         };
         style.normal.textColor = Color.magenta;
@@ -54,12 +54,13 @@ public class DetectionRectDraw : MonoBehaviour
         foreach (var info in detector.Detect(Camera.main))
         {
             if (!info.visibleInFrame) continue;
-            GUI.Label(new Rect(info.min.x * Camera.main.pixelWidth, Camera.main.pixelHeight - info.max.y * Camera.main.pixelHeight, 200, 200), info.className + " b: " + (Math.Round(info.fill * 100f, 2) + "%").ToString(), style);
-            GUI.Label(new Rect(info.min.x * Camera.main.pixelWidth, Camera.main.pixelHeight - info.max.y * Camera.main.pixelHeight+ style.fontSize, 200, 200), "d: " + Math.Round(info.distance, 1) + "m", style);
-            GUI.Label(new Rect(info.min.x * Camera.main.pixelWidth, Camera.main.pixelHeight - info.max.y * Camera.main.pixelHeight+ style.fontSize*2, 200, 200), "v: " + info.visible.ToString(), style);
-            GUI.Label(new Rect(info.min.x * Camera.main.pixelWidth, Camera.main.pixelHeight - info.max.y * Camera.main.pixelHeight+ style.fontSize*3, 200, 200), "c: " + Math.Round(info.colorPercentVisible*100f, 2) + "%", style);
-            GUI.Label(new Rect(info.min.x * Camera.main.pixelWidth, Camera.main.pixelHeight - info.max.y * Camera.main.pixelHeight+ style.fontSize*4, 200, 200), "i: " + info.includeInDataset.ToString(), style);
-            GUI.Label(new Rect(info.min.x * Camera.main.pixelWidth, Camera.main.pixelHeight - info.max.y * Camera.main.pixelHeight+ style.fontSize*5, 200, 200), "f: " + RenderSettings.fogDensity.ToString(), style);
+            GUI.Label(new Rect(info.min.x * Camera.main.pixelWidth, Camera.main.pixelHeight - info.max.y * Camera.main.pixelHeight, 200, 200), info.className, style);
+            GUI.Label(new Rect(info.min.x * Camera.main.pixelWidth, Camera.main.pixelHeight - info.max.y * Camera.main.pixelHeight + style.fontSize, 200, 200), "b: " + (Math.Round(info.fill * 100f, 2) + "%").ToString(), style);
+            GUI.Label(new Rect(info.min.x * Camera.main.pixelWidth, Camera.main.pixelHeight - info.max.y * Camera.main.pixelHeight + style.fontSize*2, 200, 200), "d: " + Math.Round(info.distance, 1) + "m", style);
+            GUI.Label(new Rect(info.min.x * Camera.main.pixelWidth, Camera.main.pixelHeight - info.max.y * Camera.main.pixelHeight + style.fontSize*3, 200, 200), "v: " + info.visible.ToString(), style);
+            GUI.Label(new Rect(info.min.x * Camera.main.pixelWidth, Camera.main.pixelHeight - info.max.y * Camera.main.pixelHeight + style.fontSize*4, 200, 200), "c: " + Math.Round(info.colorPercentVisible*100f, 2) + "%", style);
+            GUI.Label(new Rect(info.min.x * Camera.main.pixelWidth, Camera.main.pixelHeight - info.max.y * Camera.main.pixelHeight + style.fontSize*5, 200, 200), "i: " + info.includeInDataset.ToString(), style);
+            GUI.Label(new Rect(info.min.x * Camera.main.pixelWidth, Camera.main.pixelHeight - info.max.y * Camera.main.pixelHeight + style.fontSize*6, 200, 200), "f: " + (Math.Round(RenderSettings.fogDensity, 3)).ToString(), style);
         }
     }
 
