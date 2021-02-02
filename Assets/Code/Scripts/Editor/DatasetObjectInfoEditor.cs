84daafcb1b9b97cf0ec4c5401385c1ce92683b0d
Vectro <miki87278@gmail.com>
2021-02-03 00:50:41 +0100

    Improved editor scripts
    Added GUI buttons for pregenerating test points

--- Assets/Code/Scripts/Editor/DatasetObjectInfoEditor.cs
+++ Assets/Code/Scripts/Editor/DatasetObjectInfoEditor.cs
@@ -11,22 +11,30 @@ public class DatasetObjectInfoEditor : Editor
         DrawDefaultInspector();
         DatasetObjectInfo script = (DatasetObjectInfo)target;
 
-
-        if (GUILayout.Button("Clear test points"))
+        if (GUILayout.Button("Clear test points") && EditorUtility.DisplayDialog("Confirmation", "Do you want to clear test points?", "Clear", "Cancel"))
         {
             script.testPoints = null;
         }
-        if (GUILayout.Button("Regenerate uniform points"))
+
+        if (GUILayout.Button("Regenerate uniform points") && EditorUtility.DisplayDialog("Confirmation", "Do you want to regenerate?", "Regenerate", "Cancel"))
         {
             script.GenerateTestPointsUniform(script.testPointsNum);
             EditorUtility.DisplayDialog("Generation", "Done regenerating test points", "0k");
         }
-        if (GUILayout.Button("Regenerate non-uniform points"))
+
+        if (GUILayout.Button("Regenerate non-uniform points") && EditorUtility.DisplayDialog("Confirmation", "Do you want to regenerate?", "Regenerate", "Cancel"))
         {
             script.GenerateTestPointsNonUniform(script.testPointsNum);
             EditorUtility.DisplayDialog("Generation", "Done regenerating test points", "0k");
         }
+
         if (script.testPoints == null || script.testPoints.Count == 0)
             EditorGUILayout.HelpBox("Test points should be generated", MessageType.Info);
+
+        int triangles = 0;
+        foreach (MeshFilter meshFilter in script.gameObject.GetComponentsInChildren<MeshFilter>())
+            triangles += meshFilter.sharedMesh.triangles.Length;
+        if (triangles > 9000)
+        EditorGUILayout.HelpBox("Triangles count over 9000: " + triangles + "\nPregeneration is essential!" , MessageType.Warning);
     }
 }
