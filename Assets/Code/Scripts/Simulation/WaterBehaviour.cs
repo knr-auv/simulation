843302fcbf5968b2671d9a3156dc343c3cbdc2d3
Vectro <miki87278@gmail.com>
2021-02-03 20:00:23 +0100

    Optimized water behaviour

--- Assets/Code/Scripts/Simulation/WaterBehaviour.cs
+++ Assets/Code/Scripts/Simulation/WaterBehaviour.cs
@@ -22,8 +22,8 @@ public class WaterBehaviour : MonoBehaviour
         if (transform.position.y < 0)
         {
             Vector3 buoyancyForce = new Vector3(0, waterDensity * gravityConstant * volume, 0);
-            Vector3 T1 = rigidbody.velocity.normalized * -1f * K1 * rigidbody.velocity.magnitude;
-            Vector3 T2 = rigidbody.velocity.normalized * -1f * K2 * rigidbody.velocity.sqrMagnitude;
+            Vector3 T1 = -rigidbody.velocity * K1;
+            Vector3 T2 = -rigidbody.velocity * K2 * rigidbody.velocity.magnitude;
             float reynoldsNumber = T2.magnitude / T1.magnitude;
             Vector3 dragForce = reynoldsNumber < 1 ? T1 : T2;
 
@@ -32,8 +32,8 @@ public class WaterBehaviour : MonoBehaviour
             volumeCenterForce += dragForce;
             rigidbody.AddForceAtPosition(volumeCenterForce, transform.position + transform.rotation * volumeCenterOffset, ForceMode.Force);
 
-            Vector3 T1rot = rigidbody.angularVelocity.normalized * -1f * K1Rot * rigidbody.angularVelocity.magnitude;
-            Vector3 T2rot = rigidbody.angularVelocity.normalized * -1f * K2Rot * rigidbody.angularVelocity.sqrMagnitude;
+            Vector3 T1rot = -rigidbody.angularVelocity * K1Rot;
+            Vector3 T2rot = -rigidbody.angularVelocity * K2Rot * rigidbody.angularVelocity.magnitude;
             float reynoldsNumberRot = T2rot.magnitude / T1rot.magnitude;
             Vector3 dragTorque = reynoldsNumberRot < 1 ? T1rot : T2rot;
             rigidbody.AddTorque(dragTorque, ForceMode.Force);//TODO:
